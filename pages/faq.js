ab29035d8c742f027471b6f53f6710667e0f9370
Vanessa <machado.vanessa32@gmail.com>
2022-02-03 13:53:37 -0300

    Subindo alteraçoes

---
+++ pages/faq.js
@@ -0,0 +1,13 @@
+import Link from '../src/components/Link'
+
+export default function FAQpage(){
+    return(
+        <div>
+             <h1> Alura Cases - Páginas de Perguntas faq</h1> 
+            <Link href = "/">
+             Ir para o Home
+            </Link>
+             
+        </div>    
+    );
+}
