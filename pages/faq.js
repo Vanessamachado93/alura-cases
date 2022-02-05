9bc4820f771a35cc9042180633503014db5f2546
Vanessa <machado.vanessa32@gmail.com>
2022-02-05 10:12:45 -0300

    teste

--- pages/faq.js
+++ pages/faq.js
@@ -1,13 +1,10 @@
-import Link from '../src/components/Link'
+import Link from "../src/components/Link";
 
-export default function FAQpage(){
-    return(
-        <div>
-             <h1> Alura Cases - Páginas de Perguntas faq</h1> 
-            <Link href = "/">
-             Ir para o Home
-            </Link>
-             
-        </div>    
-    );
+export default function FAQpage() {
+  return (
+    <div>
+      <h1> Alura Cases - Páginas de Perguntas faq</h1>
+      <Link href="/">Ir para o Home</Link>
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,37 @@
-import Link from '../src/components/Link'
+import React from "react";
+import Link from "../src/components/Link";
+
+function globalState() {
+  return (
+    <style global jsx>{`
+      body {
+        font-family: sans-serif;
+      }
+    `}</style>
+  );
+}
+
+function Title({ children }) {
+  const Tag = as;
+  return (
+    <React.Fragment>
+      <Tag>{children}</Tag>
+      <style jsx>{`
+        ${Tag}
+        h2 {
+          color: red;
+        }
+      `}</style>
+    </React.Fragment>
+  );
+}
 
 export default function HomePage() {
-    return(
-        <div>
-            <h1>Alura Cases - Home Page</h1>
-            <Link href="/faq">
-            Ir para página do FAQ
-            </Link>
-         </div>
-    );
+  return (
+    <div>
+      <globalState />
+      <h1>Alura Cases - Home Page</h1>
+      <Link href="/faq">Ir para página do FAQ</Link>
+    </div>
+  );
 }
